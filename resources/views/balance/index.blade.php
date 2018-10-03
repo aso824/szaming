442b1360ba9703222cd45e75ca65ebd5087cb590
alex <alzholus@gmail.com>
2018-10-03 23:53:44 +0200

    Rename variable

--- resources/views/balance/index.blade.php
+++ resources/views/balance/index.blade.php
@@ -32,14 +32,14 @@
                 <div class="card">
                     <div class="card-header">{{ __('To whom you owe') }}</div>
                     <div class="card-body">
-                        @forelse ($balanceViewModel->getCredits() as $credit)
-                            @php /** @var \App\Models\User $credit */ @endphp
+                        @forelse ($balanceViewModel->getCredits() as $creditor)
+                            @php /** @var \App\Models\User $creditor */ @endphp
 
                             @if ($loop->first)
                                   <ul>
                             @endif
                                       <li>
-                                          {{ $credit->name }}: <span class="font-weight-bold text-danger">-{{ $balanceViewModel->formatPrice($credit->pivot->amount) }}</span>
+                                          {{ $creditor->name }}: <span class="font-weight-bold text-danger">-{{ $balanceViewModel->formatPrice($creditor->pivot->amount) }}</span>
                                       </li>
                             @if ($loop->last)
                                   </ul>
