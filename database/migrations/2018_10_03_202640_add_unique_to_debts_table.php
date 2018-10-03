0a1253db5e20b1d41f9badd242328b442211cff8
alex <alzholus@gmail.com>
2018-10-03 22:32:41 +0200

    fix styleci

--- database/migrations/2018_10_03_202640_add_unique_to_debts_table.php
+++ database/migrations/2018_10_03_202640_add_unique_to_debts_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddUniqueToDebtsTable extends Migration
 {
@@ -29,7 +29,7 @@ class AddUniqueToDebtsTable extends Migration
             $table->index(['creditor_id']);
         });
 
-        Schema::table('debts', function (Blueprint$table) {
+        Schema::table('debts', function (Blueprint $table) {
             $table->dropUnique(['creditor_id', 'debtor_id']);
         });
     }
